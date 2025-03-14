ce7af7d91e8c9e3523282ede6c3b04bd6dfc3971
Mauricio Villegas <5780272+mauvilsa@users.noreply.github.com>
2025-03-14 15:20:03 +0100

    Bump version: 0.1.0.dev1 → 0.1.0.dev2

--- deprecated_parameters/__init__.py
+++ deprecated_parameters/__init__.py
@@ -1,7 +1,7 @@
 from ._decorator import *  # noqa: F403
 from ._mypy import *  # noqa: F403
 
-__version__ = "0.1.0.dev1"
+__version__ = "0.1.0.dev2"
 __all__ = ["__version__"]
 
 from . import _decorator, _mypy
