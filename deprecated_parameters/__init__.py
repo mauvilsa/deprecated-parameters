a96628b5bd648a5e9c0b53c14ad401f4feef63a8
Mauricio Villegas <5780272+mauvilsa@users.noreply.github.com>
2026-08-26 07:56:17 +0200

    Bump version: 0.1.0.dev2 → 0.1.0

--- deprecated_parameters/__init__.py
+++ deprecated_parameters/__init__.py
@@ -3,7 +3,7 @@ from ._mypy import *  # noqa: F403
 from ._sphinx import *  # noqa: F403
 from ._stubgen import *  # noqa: F403
 
-__version__ = "0.1.0.dev2"
+__version__ = "0.1.0"
 __all__ = ["__version__"]
 
 from . import _decorator, _mypy, _sphinx, _stubgen
